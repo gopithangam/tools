f3ce2da44bf85e1b2e0df8ba2a35c4656520f603
gopithangam <gopithangam8@gmail.com>
2024-10-07 17:13:47 +0530

    Update bigfix_uninstall_clean.ps1

--- powershell/bigfix_uninstall_clean.ps1
+++ powershell/bigfix_uninstall_clean.ps1
@@ -1,7 +1,7 @@
 
 # This script is intended to remove an existing installation of BigFix so that a new install can be performed pointing to a new server or with other changes.
 # This script will uninstall BigFix, then delete the BigFix client folder if present, then delete the BigFix client registry entries if present.
-#  - Related: http://software.bigfix.com/download/bes/100/util/BESRemove-10.0.2.52.exe
+#  - Related: https://software.bigfix.com/download/bes/110/util/BESRemove11.0.2.125.exe
 
 # To download and run:
 <#
